0ef0a561824a9a5047e7ceffccf33dfdad3000dd
UMAMAHESWARI1103 <umamaheswari2t3@gmail.com>
2026-03-12 11:23:07 +0530

    Fix admin dashboard const errors and update admin screens

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.189.163.70:5000/api';
+  static const String baseUrl = 'http://10.179.12.70:5000/api';
 
   static String? _token;
 
